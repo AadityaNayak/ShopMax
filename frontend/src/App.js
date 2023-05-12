118d63be5fa3f3abff16ff4d0334a9d2eebe9066
AadityaNayak <aadityanayak7@gmail.com>
2023-05-12 23:27:59 +0530

    Added cartScreen and Route for it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProductDetails from "./screens/ProductDetails";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
@@ -12,10 +13,11 @@ function App() {
         <Header />
         <main>
           <Container>
-            <h1>ShopMax</h1>
+            {/* <h1>ShopMax</h1> */}
       <Routes>
             <Route path="/" Component={HomeScreen} exact />
             <Route path="/products/:id" Component={ProductDetails} />
+            <Route path="/cart/:id?" Component={CartScreen} />
       </Routes>
           </Container>
         </main>
