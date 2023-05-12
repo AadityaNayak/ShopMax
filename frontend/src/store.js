67448ec4cee3d96b60ce9d5c6ad0c26d1f933e45
AadityaNayak <aadityanayak7@gmail.com>
2023-05-13 00:21:23 +0530

    cartActions added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,23 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import {productListReducer, productDetailsReducer} from "./reducers/productReducer"
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducer";
 import { cartReducer } from "./reducers/cartReducer";
 
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
 const reducer = combineReducers({
-    productList : productListReducer,
-    productDetails:productDetailsReducer,
-    cart:cartReducer
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const initialState = {cart:{cartItems: cartItemsFromStorage}};
 const middleware = [thunk];
 
 const store = createStore(
