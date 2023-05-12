68e327b632946923f6d93a050058371d91f774d0
AadityaNayak <aadityanayak7@gmail.com>
2023-05-13 00:52:53 +0530

    Added code to cart screen and implemented add and remove item in cart functionalities

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstant";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstant";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   let data = await fetch(`http://localhost:8080/api/products/${id}`);
@@ -18,3 +17,12 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+    dispatch({
+      type: CART_REMOVE_ITEM,
+      payload: id,
+    });
+  
+    localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  };
