ff08022d5bf943a6148caeb18be17d09a42a03f2
AadityaNayak <aadityanayak7@gmail.com>
2023-05-13 13:34:06 +0530

    made userReducer and added it to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,6 +6,7 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducer";
 import { cartReducer } from "./reducers/cartReducer";
+import { userLoginReducer } from "./reducers/userReducer";
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
@@ -15,6 +16,7 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const initialState = {cart:{cartItems: cartItemsFromStorage}};
