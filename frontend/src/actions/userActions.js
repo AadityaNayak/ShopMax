e74cbd8c7a20ae4145c7f320c039910de9c9d488
AadityaNayak <aadityanayak7@gmail.com>
2023-05-13 15:05:15 +0530

    Added action for logout and implemented functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
   
 } from "../constants/userConstant";
 
@@ -30,3 +31,8 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
+
+export const logout = ()=>dispatch=>{
+  localStorage.removeItem('userInfo')
+  dispatch({type:USER_LOGOUT})
+}
