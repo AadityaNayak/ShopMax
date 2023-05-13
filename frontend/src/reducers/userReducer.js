794bf3b191f6c108bdfed5dfa15f8b02e6117352
AadityaNayak <aadityanayak7@gmail.com>
2023-05-13 13:50:57 +0530

    Added login action for user in userActions

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -2,7 +2,7 @@ import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
-  } from "../constants/userContants";
+  } from "../constants/userConstant";
   
   export const userLoginReducer = (state = {}, action) => {
     switch (action.type) {
@@ -12,8 +12,6 @@ import {
         return { loading: false, userInfo: action.payload };
       case USER_LOGIN_FAIL:
         return { loading: false, error: action.payload };
-      case USER_LOGOUT:
-        return {};
       default:
         return state;
     }
